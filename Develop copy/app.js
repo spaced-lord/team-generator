c541bd31ab2f477d21a87c4c471d7c7770131ddd
spaced-lord <zackarynoe@gmail.com>
2021-01-25 16:30:40 -0700

    html render

--- Develop copy/app.js
+++ Develop copy/app.js
@@ -93,26 +93,33 @@ let storeEmp = (employees, nextEmp) => {
 
   empId++;
 
- 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+  //  SWITCH STATEMENT
+  switch (nextEmp) {
+    case "Manager":
+      newEmpQuestion("What is the employee's office number?", "Manager");
+      break;
+    case "Engineer":
+      newEmpQuestion("What is the employee's Github username?", "Engineer");
+      break;
+    case "Intern":
+      newEmpQuestion("Which school did the employee attend?", "Intern");
+      break;
+    case "None to add":
+      let htmlRenderer = render(empArray);
+      writeHTML(htmlRenderer);
+    default:
+      return;
+  }
+};
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// FS WRITE HTML
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+let writeHTML = (htmlRenderer) => {
+  fs.writeFile(outputPath, htmlRenderer, (err) => {
+    if (err) {
+      console.log("ERROR!");
+    } else {
+      console.log("Rendering File");
+    }
+  });
+};
